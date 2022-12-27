6c276daeb94bb87867e12a9a8152a20cbe94b1be
Karol Sójko <karolsojko@protonmail.com>
2022-12-27 09:17:20 +0100

    fix: add generating default keys

--- server.sh
+++ server.sh
@@ -148,6 +148,13 @@ case "$COMMAND" in
         ;;
     esac
     ;;
+  'generate-keys' )
+    sed -i '' "s/auth_jwt_secret/$(openssl rand -hex 32)/g" .env
+    sed -i '' "s/legacy_jwt_secret/$(openssl rand -hex 32)/g" docker/auth.env
+    sed -i '' "s/secret_key/$(openssl rand -hex 32)/g" docker/auth.env
+    sed -i '' "s/server_key/$(openssl rand -hex 32)/g" docker/auth.env
+    sed -i '' "s/secret/$(openssl rand -hex 32)/g" docker/auth.env
+    ;;
   'test' )
     waitForServices
 
